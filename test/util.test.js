d83c2bfb00c1d0e4ba1ace03adc390322e37a9c6
Josh Hundley <josh.hundley@gmail.com>
2020-07-31 15:58:49 -0700

    update tests

--- test/util.test.js
+++ test/util.test.js
@@ -1,4 +1,4 @@
-let sea = require('../build/bundle.js');
+let utils = require('../build/bundle.js');
 let chai = require('chai');
 let assert = require('assert');
 let crypto = require('crypto');
@@ -11,11 +11,11 @@ function sha(data, type) {
   return generator.digest('buffer')
 }
 
-describe('SEA', function () {
+describe('isocrypt-utils', function () {
 
   describe('sha1', () => {
     it('should generate a sha1 hash', async () => {
-      let hash = await sea.sha1('foo');
+      let hash = await utils.sha1('foo');
       let nodehash = sha('foo', 'sha1');
       Buffer.compare(hash, nodehash).should.equal(0);
     });
@@ -23,7 +23,7 @@ describe('SEA', function () {
 
   describe('sha256', () => {
     it('should generate a sha256 hash', async () => {
-      let hash = await sea.sha256('foo');
+      let hash = await utils.sha256('foo');
       let nodehash = sha('foo', 'sha256');
       Buffer.compare(hash, nodehash).should.equal(0);
     });
@@ -31,7 +31,7 @@ describe('SEA', function () {
 
   describe('pair', () => {
     it('should generate a keypair', async () => {
-      let pair = await sea.pair();
+      let pair = await utils.pair();
 
       pair.should.have.property('pub');
       pair.should.have.property('priv');
@@ -42,25 +42,25 @@ describe('SEA', function () {
 
   describe('encrypt', () => {
     it('should encrypt data with a private key', async () => {
-      let pair = await sea.pair();
-      let encrypted = await sea.encrypt('Hello world', pair.epriv);
+      let pair = await utils.pair();
+      let encrypted = await utils.encrypt('Hello world', pair.epriv);
     });
   });
 
   describe('decrypt', () => {
     it('should decrypt data that has been encrypted', async () => {
-      let pair = await sea.pair();
-      let encrypted = await sea.encrypt('Hello world', pair.epriv);
-      let decrypted = await sea.decrypt(encrypted, pair.epriv);
+      let pair = await utils.pair();
+      let encrypted = await utils.encrypt('Hello world', pair.epriv);
+      let decrypted = await utils.decrypt(encrypted, pair.epriv);
       decrypted.should.equal('Hello world');
     });
   });
 
   describe('sign messages', () => {
     it('should produce a signature', async () => {
-      let pair = await sea.pair();
-      let encrypted = await sea.encrypt('Hello world', pair.priv);
-      let signature = await sea.sign(encrypted, pair);
+      let pair = await utils.pair();
+      let encrypted = await utils.encrypt('Hello world', pair.priv);
+      let signature = await utils.sign(encrypted, pair);
       signature.should.be.a('string');
     });
   });
@@ -68,19 +68,19 @@ describe('SEA', function () {
   describe('verify message', () => {
     it('should verify signature', async () => {
       let message = 'My message';
-      let pair = await sea.pair();
-      let signature = await sea.sign(message, pair);
-      let check = await sea.verify(message, signature, pair.pub);
+      let pair = await utils.pair();
+      let signature = await utils.sign(message, pair);
+      let check = await utils.verify(message, signature, pair.pub);
       check.should.equal(true);
     });
 
     it('should fail signature if signed by other', async () => {
       let message = 'My message';
-      let mypair = await sea.pair();
-      let otherpair = await sea.pair();
-      let signature = await sea.sign(message, otherpair);
+      let mypair = await utils.pair();
+      let otherpair = await utils.pair();
+      let signature = await utils.sign(message, otherpair);
       try {
-        await sea.verify(message, signature, mypair.pub);
+        await utils.verify(message, signature, mypair.pub);
       } catch(error) {
         assert(true);
         return;
